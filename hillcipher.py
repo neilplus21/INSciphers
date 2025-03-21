84ca51037c3936e7a4c936c05bcb32b91dbcb8dc
Neil <143861570+neilplus21@users.noreply.github.com>
2025-03-21 08:22:24 +0530

    Update hillcipher.py

--- hillcipher.py
+++ hillcipher.py
@@ -1,72 +1,42 @@
-def text_to_numbers(text):
+import numpy as np
+
+l = list(map(int, input("Enter the key matrix : ").split()))
+keyMatrix = np.array(l).reshape(2,2)
+
+det = keyMatrix[0,0] * keyMatrix[1,1] - keyMatrix[0,1] * keyMatrix[1,0]
+det = pow(int(det), 1, 26)  # Compute determinant mod 26
+
+detInverse = pow(det, -1, 26)  # Compute modular inverse of determinant
+
+keyInverse = np.array([[keyMatrix[1,1], -keyMatrix[0,1]], [-keyMatrix[1,0], keyMatrix[0,0]]])
+keyInverse = (detInverse * keyInverse) % 26
+
+def text_to_num(text):
     return [ord(char) - ord('A') for char in text]
 
-def numbers_to_text(numbers):
-    return ''.join(chr(num + ord('A')) for num in numbers)
+def num_to_text(nums):
+    return "".join([chr(num + ord('A')) for num in nums])
 
-def matrix_multiply(matrix, text_matrix):
-    result = []
-    for row in matrix:
-        value = (row[0] * text_matrix[0] + row[1] * text_matrix[1]) % 26
-        result.append(value)
-    return result
+def encrypt(plainText):
+    plainText = plainText.upper()
+    if len(plainText) % 2 == 1:  # If length is odd, add a padding character
+        plainText += 'X'
+    cipherText = ""
+    for i in range(0, len(plainText), 2):
+        block = np.array(text_to_num(plainText[i:i+2]))
+        encryptedBlock = np.dot(keyMatrix, block)%26  # Convert result to integer
+        cipherText += num_to_text(encryptedBlock)
+    return cipherText
 
-def hill_encrypt(plaintext, key_matrix):
-    if len(plaintext) % 2 != 0:
-        plaintext += 'X'
-    
-    print("\nPlaintext:", plaintext)
-    
-    plaintext_numbers = text_to_numbers(plaintext)
-    print("\nPlaintext Matrix (M):")
-    for i in range(0, len(plaintext_numbers), 2):
-        print(plaintext_numbers[i:i+2])
+def decrypt(cipherText):
+    cipherText = cipherText.upper()
+    plainText = ""
+    for i in range(0, len(cipherText), 2):
+        block = np.array(text_to_num(cipherText[i:i+2]))
+        decryptedBlock = np.dot(keyInverse, block)% 26  # Convert result to integer
+        plainText += num_to_text(decryptedBlock)
+    return plainText
 
-    ciphertext_numbers = []
-    for i in range(0, len(plaintext_numbers), 2):
-        ciphertext_numbers.extend(matrix_multiply(key_matrix, plaintext_numbers[i:i+2]))
-
-    print("\nCiphertext Matrix (N):")
-    for i in range(0, len(ciphertext_numbers), 2):
-        print(ciphertext_numbers[i:i+2])
-    
-    return numbers_to_text(ciphertext_numbers)
-
-def matrix_inverse(matrix):
-    det = (matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]) % 26
-    det_inv = pow(det, -1, 26)
-
-    inv_matrix = [
-        [(matrix[1][1] * det_inv) % 26, (-matrix[0][1] * det_inv) % 26],
-        [(-matrix[1][0] * det_inv) % 26, (matrix[0][0] * det_inv) % 26]
-    ]
-    return inv_matrix
-
-def hill_decrypt(ciphertext, key_matrix):
-    print("\nCiphertext:", ciphertext)
-
-    ciphertext_numbers = text_to_numbers(ciphertext)
-    print("\nCiphertext Matrix (N):")
-    for i in range(0, len(ciphertext_numbers), 2):
-        print(ciphertext_numbers[i:i+2])
-
-    inverse_matrix = matrix_inverse(key_matrix)
-
-    plaintext_numbers = []
-    for i in range(0, len(ciphertext_numbers), 2):
-        plaintext_numbers.extend(matrix_multiply(inverse_matrix, ciphertext_numbers[i:i+2]))
-
-    print("\nDecrypted Matrix (M'):")
-    for i in range(0, len(plaintext_numbers), 2):
-        print(plaintext_numbers[i:i+2])
-
-    return numbers_to_text(plaintext_numbers)
-
-key_matrix = [[3, 3], [2, 5]]
-plaintext = "HELLOCHINGCHONG"
-
-ciphertext = hill_encrypt(plaintext, key_matrix)
-print("\nEncrypted Text:", ciphertext)
-
-decrypted_text = hill_decrypt(ciphertext, key_matrix)
-print("\nDecrypted Text:", decrypted_text)
+plainText = input("Enter a message : ")
+print("Encrypted : ", encrypt(plainText))
+print("Decrypted : ", decrypt(encrypt(plainText)))
